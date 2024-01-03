6cd5313e5db80d3daac68266f12747e5324b786d
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2024-01-03 13:14:31 -0600

    Added modal functionality to nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,18 @@
 import blackLogo from "../images/wt-black-nobg.png";
 import colorLogo from "../images/RedVersion-removebg-preview.png";
 
-const Nav = ({ minimal, authToken }) => {
+const Nav = ({ minimal, authToken, setShowModal, showModal }) => {
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
   return (
     <nav>
       <div className="logo-container">
         <img className="logo" src={minimal ? colorLogo : blackLogo} />
       </div>
 
-      {!authToken && !minimal && <button className="nav-btn">Login</button>}
+      {!authToken && !minimal && <button className="nav-btn" onClick={handleClick} disabled={showModal}>Login</button>}
     </nav>
   );
 };
