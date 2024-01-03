01adec291d5f0227ac7713c182adeaa4e81d3c99
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2024-01-03 13:44:16 -0600

    Added modal form outline

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -1,16 +1,64 @@
+import { useState } from "react";
 
 const AuthModal = ({ setShowModal }) => {
-  
+  const [ email, setEmail ] = useState(null);
+  const [ password, setPassword ] = useState(null);
+  const [ confirmPassword, setConfirmPassword] = useState(null);
+  const [ error, setError ] = useState(null);
+
+  console.log(email, password, confirmPassword)
+
   const handleClick = () => {
     setShowModal(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
   }
+
+  const isSignedUp = true;
 
   return (
     <div className="auth-modal">
-      <div onClick={handleClick}>ⓧ</div>
+      <div className="close-icon" onClick={handleClick}>
+        ⓧ
+      </div>
+      <h2>{isSignedUp ? "CREATE ACCOUNT" : "LOG IN"}</h2>
+      <p>
+        By clicking 'LOG IN', you agree to our terms. Learn how we process your
+        data in our Privacy Policy and Cookie Policy.
+      </p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="email"
+          required={true}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="password"
+          required={true}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password-check"
+          id="password-check"
+          name="password-check"
+          placeholder="Confirm Password"
+          required={true}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <input className="secondary-button" type="submit"/>
+        <p>{error}</p>
+      </form>
       AUTH MODAL
     </div>
-  )
-}
+  );
+};
 
-export default AuthModal
+export default AuthModal;
