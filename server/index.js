b9605a8dbaf99fbef2511cf753fb4ca588f85609
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2024-01-24 17:26:20 -0600

    Started on backend framework

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const PORT = 8000;
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const uri = 'mongodb+srv://root:mongodb115@cluster0.9h9ocqu.mongodb.net/'
+
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.json('Hello to my app!');
+})
+
+app.post('/signup', (req, res) => {
+  res.json('Hello to my app!');
+})
+
+app.get('/users', async (req, res) => {
+  const client = new MongoClient(uri)
+
+  try {
+    await client.connect()
+    const database = client.db('tinderClone');
+    const users = database.collection('users');
+
+    const returnedUsers = await users.find().toArray();
+    res.send(returnedUsers);
+  } finally {
+    await client.close();
+  }
+})
+
+
+app.listen(PORT, () => console.log('Server running on PORT ' + PORT))
