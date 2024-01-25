b5ec12e78b36dd7281cc1c079934b331ee01ffaf
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2024-01-25 17:34:19 -0600

    Adding user onboarding logic and database connections

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const PORT = 8000;
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const { v4: uuid } = require('uuid');
 const uri = 'mongodb+srv://root:mongodb115@cluster0.9h9ocqu.mongodb.net/'
 
 
@@ -10,8 +11,34 @@ app.get('/', (req, res) => {
   res.json('Hello to my app!');
 })
 
-app.post('/signup', (req, res) => {
-  res.json('Hello to my app!');
+app.post('/signup', async (req, res) => {
+  const client = new MongoClient(uri);
+  const { email, password } = req.body;
+
+  const generateUserId = uuidv4();
+  const hashedpassword = await bcrypt.hash(password, 10);
+
+  try {
+    client.connect();
+    const database = client.db('tinderClone');
+    const users = database.collection('users');
+
+    const existingUser = users.findOne({ email });
+
+    if (existingUser) {
+      return res.status(409).send("User already exists. Please login!");
+    }
+
+    const sanitizedEmail = email.toLowerCase();
+    const data = {
+      user_id: generateUserId,
+      email: sanitizedEmail,
+      hashed_password: hashedpassword
+    }
+
+    const insertedUser = await users.insertOne(data);
+  }
+
 })
 
 app.get('/users', async (req, res) => {
