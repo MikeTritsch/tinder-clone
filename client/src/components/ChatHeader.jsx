e0646b15278c296451a0867095023ceca54f002d
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2024-02-01 17:20:57 -0600

    Currently debugging prop/hook work in rendering usernames to logged in users

--- client/src/components/ChatHeader.jsx
+++ client/src/components/ChatHeader.jsx
@@ -1,11 +1,11 @@
-const ChatHeader = () => {
+const ChatHeader = ({ user }) => {
   return (
     <div className="chat-container-header">
       <div className="profile">
         <div className="image-container">
           <img src=""/>
         </div>
-        <h3>UserName</h3>
+        <h3>{user.first_name}</h3>
       </div>
       <i className="log-out-icon">115</i>
     </div>
