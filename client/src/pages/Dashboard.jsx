5049fda4f56d23ab2fc66813aee766a9f4093ec2
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2024-02-19 16:05:47 -0600

    Removed character list, replaced it with DB names and info

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -7,6 +7,7 @@ import axios from "axios";
 const Dashboard = () => {
   const [user, setUser] = useState(null);
   const [genderedUsers, setGenderedUsers] = useState(null);
+  const [lastDirection, setLastDirection] = useState();
   const [ cookies, setCookie, removeCookie ] = useCookies(['user']);
 
   const userId = cookies.UserId;
@@ -48,23 +49,6 @@ useEffect(() => {
   console.log('gendered Users', genderedUsers);
 
 
-  const characters = [
-    {
-      name: "Richard Hendricks",
-      url: "https://imgur.com/MWAcQRM.jpg",
-    },
-    {
-      name: "Erlich Bachman",
-      url: "https://imgur.com/wDmRJPc.jpg",
-    },
-    {
-      name: "Monica Hall",
-      url: "https://imgur.com/OckVkRo.jpg",
-    },
-  ];
-
-  const [lastDirection, setLastDirection] = useState();
-
   const swiped = (direction, nameToDelete) => {
     console.log("removing: " + nameToDelete);
     setLastDirection(direction);
@@ -79,21 +63,23 @@ useEffect(() => {
       <ChatContainer user={user}/>
       <div className="swipe-container">
         <div className="card-container">
-          {characters.map((character) => (
+
+          {genderedUsers && genderedUsers.map((genderedUser) => (
             <TinderCard
               className="swipe"
-              key={character.name}
-              onSwipe={(dir) => swiped(dir, character.name)}
-              onCardLeftScreen={() => outOfFrame(character.name)}
+              key={genderedUser.first_name}
+              onSwipe={(dir) => swiped(dir, genderedUser.first_name)}
+              onCardLeftScreen={() => outOfFrame(genderedUser.first_name)}
             >
               <div
-                style={{ backgroundImage: "url(" + character.url + ")" }}
+                style={{ backgroundImage: "url(" + genderedUser.url + ")" }}
                 className="card"
               >
-                <h3>{character.name}</h3>
+                <h3>{genderedUser.first_name}</h3>
               </div>
             </TinderCard>
           ))}
+
           <div className="swipe-info">
             {lastDirection ? <p>You swiped {lastDirection}</p> : <p />}
           </div>
